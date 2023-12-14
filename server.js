0dd2e8b2441781ad1b353bda3dedac5a2ab4a583
DarvinDheena <darvindheena98@gmail.com>
2023-12-14 07:42:15 +0530

    adding html in server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(request,response)=>{
-    response.send('Hello Darvin');
+    response.send('<h1>Hello Darvin<h1>');
 })
 
 // create a array of object
