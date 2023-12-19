4e90cda9550043f4fb74c1570be0288fd7b5920d
DarvinDheena <darvindheena98@gmail.com>
2023-12-19 11:00:28 +0530

    day-3 initial setup

--- server.js
+++ server.js
@@ -1,44 +1,14 @@
 const express = require('express');
-const { request } = require('http');
-
 const app = express();
-// endpoint to / path
-app.get('/',(request,response)=>{
-    response.send('<h1>Notes Application<h1>');
-})
 
-// endponit to get the notes
+// create a middleware
 
-app.get('/notes',(request,response)=>{
-    response.json(notes);
-})
+app.use(express.json());
 
-// create a array of object
-
-let notes = [
-    {
-        "id":1,
-        "content":'node js',
-        "important": true
-    },
-    {
-        "id":2,
-        "content":'open source',
-        "important": false
-    },
-    {
-        "id":3,
-        "content":'server side an web development',
-        "important": true
-    }
-]
-
-
-// create a port
-
+const HOSTNAME = '127.0.0.1';
 const PORT = 3001 ;
 // let listen the server
 
 app.listen(PORT,()=>{
-    console.log('server running on port number 3001')
+    console.log(`server running at http://${HOSTNAME}:${HOST}`);
 });
